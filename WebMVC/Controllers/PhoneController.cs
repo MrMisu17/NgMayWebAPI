8ccd766f1d8f64285ccdc9c058ad087eb0ab3e07
MrMisu17 <55481626+MrMisu17@users.noreply.github.com>
2020-07-03 18:12:56 +0700

    FIrst

---
+++ WebMVC/Controllers/PhoneController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using WebMVC.Models;
+
+namespace WebMVC.Controllers
+{
+    public class PhoneController : Controller
+    {
+        // GET: Phone
+        public ActionResult Index()
+        {
+            IEnumerable<mvcPhoneModel> phoList;
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Phone").Result;
+            phoList = response.Content.ReadAsAsync<IEnumerable<mvcPhoneModel>>().Result;
+
+            return View(phoList);
+        }
+
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new mvcPhoneModel());
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Phone/" + id.ToString()).Result;
+                return View(response.Content.ReadAsAsync<mvcPhoneModel>().Result);
+            }
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(mvcPhoneModel emp)
+        {
+            if (emp.ID == 0)
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PostAsJsonAsync("Phone", emp).Result;
+                TempData["SuccessMessage"] = "Saved Successfully";
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PutAsJsonAsync("Phone/" + emp.ID, emp).Result;
+                TempData["SuccessMessage"] = "Update Successfully";
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            HttpResponseMessage response = GlobalVariables.WebApiClient.DeleteAsync("Phone/" +id.ToString()).Result;
+            TempData["SuccessMessage"] = "Delete Successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebMVC/Models/mvcPhoneModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebMVC.Models
+{
+    public class mvcPhoneModel
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "This Field is Required")]
+        public string Model { get; set; }
+        public Nullable<decimal> Price { get; set; }
+        public string GeneralNote { get; set; }
+    }
+}
